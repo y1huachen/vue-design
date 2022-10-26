ffcf6f80f74396ae137168083d11d9d95eb95fe7
waiting <waiting@waiting-mbp.local>
2022-10-26 17:02:35 +0800

    update deploy file

--- build/deploy.sh
+++ build/deploy.sh
@@ -7,11 +7,11 @@ npm run build
 cd docs/.vuepress/dist
 
 git init
-git config user.name 'HcySunYang'
-git config user.email 'HcySunYang@outlook.com'
+git config user.name 'y1huachen'
+git config user.email '2773433112@qq.com'
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:HcySunYang/vue-design.git master:gh-pages
+git push -f git@github.com:y1huachen/vue-design.git elegant:gh-pages
 
 cd -
